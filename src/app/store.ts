0fb85e6dfa7128aea0cc92d99a05a71bba540175
Marcelo Mena <marcelomena010@gmail.com>
2021-06-27 01:00:25 -0300

    chore: remove unused redux store and reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import reviewReducer from 'components/review/reviewReducer'
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         review: reviewReducer
     }
 })
